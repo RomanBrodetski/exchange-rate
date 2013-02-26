25bf778f973acfacb0169d95c75e49ef7a807bf5
Roman Brodetskiy <roman.brodetskiy@e-legion.com>
2013-02-26 21:00:24 +0400

    at method & store

--- exchange_rate.rb
+++ exchange_rate.rb
@@ -3,22 +3,39 @@ require 'nokogiri'
 require 'date'
 require 'yaml'
 
-module ExchangeRate
+class ExchangeRate
   RatesUrl = 'http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml'
 
-  def self.fetch_rates
+  def at date, from, to
+    puts rates.class
+    rates[date][to.to_sym] / rates[date][from.to_sym]
+  end
+
+  def rates
+    update_rates unless File.exists? ("store.yaml")
+    @rates ||= YAML.load_file("store.yaml")
+  end
+
+  def update_rates
+    File.open("store.yaml", "w") do |file|
+      file.write @rates = resolve_rates_from_ecb.to_yaml
+    end
+  end
+
+  def resolve_rates_from_ecb
     xml_data = Net::HTTP.get_response(URI.parse(RatesUrl)).body
     doc = Nokogiri::XML(xml_data).remove_namespaces!
     rates = {}
-
     doc.xpath('//Cube/Cube[Cube]').each do |date_rates|
       time = Date.strptime(date_rates.attr('time'), '%Y-%m-%d')
       rates[time] = date_rates.children.map do |rate|
         {rate.attr('currency').to_sym => rate.attr('rate').to_f}
-      end.inject :merge
+      end.inject(:merge).merge(:EUR => 1.0)
     end
-
     rates
   end
 
 end
+
+# a = ExchangeRate.new
+puts ExchangeRate.new.at(Date.today, :EUR, :USD)
